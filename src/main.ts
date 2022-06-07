fe9268e4bce92dbaf4c836a347cf320ca1eee6f2
John Opaluwa <johnopaluwa@gmail.com>
2022-06-08 00:00:42 +0200

    add time out

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+    },
+    bodyParser: false,
+  });
+
+  const server = await app.listen(2001);
+  server.setTimeout(1200000);
 }
 bootstrap();
